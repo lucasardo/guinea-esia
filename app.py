e18b908a3ae3d678ba8ccb21c68f8e0edffc4985
Luca Sardo <77811564+lucasardo@users.noreply.github.com>
2025-02-18 23:18:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -131,8 +131,8 @@ Rockbursts are defined as damage to an excavation that occurs suddenly and is as
 
 <b>Damage Mechanisms</b>
 The damage mechanisms of rockbursts focus on the mechanisms that accelerate fragments of rock at the location of the observed damage. Two key damage mechanisms are: 
-Local conversion of stored strain energy into kinetic energy due to rapid brittle fracturing. This damage mechanism is sometimes referred to generally as strainbursting (Source_Document_Name_1)
-Momentum transfer due to interaction with seismic waves (i.e., ground motion). This damage mechanism is commonly referred to as shakedown, seismic ejection, or seismically induced falls of ground (Source_Document_Name_2). 
+Local conversion of stored strain energy into kinetic energy due to rapid brittle fracturing. This damage mechanism is sometimes referred to generally as strainbursting (Source_Document_Title_1)
+Momentum transfer due to interaction with seismic waves (i.e., ground motion). This damage mechanism is commonly referred to as shakedown, seismic ejection, or seismically induced falls of ground (Source_Document_Title_2). 
 These two mechanisms are different, working either singularly or in combination.  
 
 <b>Trigger Mechanisms</b>
@@ -152,27 +152,27 @@ Cable bolts are a type of ground support commonly used in mining and civil engin
 
 General Description: 
 
- - A cable bolt is a flexible tendon comprised of steel strands that are woven together (Source_Document_Name_1).  
- - The cable is inserted into boreholes and grouted in place using cement or resin grout (Source_Document_Name_2). The grout enables transfer of load from the rock mass to the cable, which is mainly intended to enhance the tensile strength of the rock mass in the direction parallel to the installation (Source_Document_Name_1).  
- - Cables can be fitted with plates or left unplated (Source_Document_Name_3)
- - Cables can be tensioned to provide active support pressure (Source_Document_Name_4)
- - Common diameters of cable bolts used in mining applications include 15.2 mm (0.6 inch) and 17.8 mm (0.7 inch). In some cases, two cables are installed in a single borehole (Source_Document_Name_2).  
- - Most cables are plain strand, but modified versions are available which are generally intended to enhance the load transfer between the rock mass to the cable (e.g., bird caged, bulbed strand) (Source_Document_Name_3). 
+ - A cable bolt is a flexible tendon comprised of steel strands that are woven together (Source_Document_Title_1).  
+ - The cable is inserted into boreholes and grouted in place using cement or resin grout (Source_Document_Title_2). The grout enables transfer of load from the rock mass to the cable, which is mainly intended to enhance the tensile strength of the rock mass in the direction parallel to the installation (Source_Document_Title_1).  
+ - Cables can be fitted with plates or left unplated (Source_Document_Title_3)
+ - Cables can be tensioned to provide active support pressure (Source_Document_Title_4)
+ - Common diameters of cable bolts used in mining applications include 15.2 mm (0.6 inch) and 17.8 mm (0.7 inch). In some cases, two cables are installed in a single borehole (Source_Document_Title_2).  
+ - Most cables are plain strand, but modified versions are available which are generally intended to enhance the load transfer between the rock mass to the cable (e.g., bird caged, bulbed strand) (Source_Document_Title_3). 
 
 <b>Advantages:</b>
- - In underground excavations, a key advantage of cable bolts over stiff rock bolts is that they can be installed at lengths that are much larger than the diameter of the excavation, which enables installation into deeper and more stable rock masses (Source_Document_Name_4). 
- - Cable bolt installation can be largely automated with long lengths of cable available on reels (Source_Document_Name_1)
+ - In underground excavations, a key advantage of cable bolts over stiff rock bolts is that they can be installed at lengths that are much larger than the diameter of the excavation, which enables installation into deeper and more stable rock masses (Source_Document_Title_4). 
+ - Cable bolt installation can be largely automated with long lengths of cable available on reels (Source_Document_Title_1)
 
 <b>Disadvantages:</b>
- - Cables are flexible and therefore provide minimal bending resistance (Source_Document_Name_2)
+ - Cables are flexible and therefore provide minimal bending resistance (Source_Document_Title_2)
  - The steels used to manufacture cable bolts are prone to corrosion which may require additional protective coatings (e.g., plastic coatings)
  - The effectiveness of cable bolts can be seriously impaired by ineffective bonding between the cable and the grout 
  - Cables are less stiff and therefore allow more displacement of the rock mass prior to the load capacity being engaged 
 
 <b>Examples of Use in Mining:</b>
- - Stabilizing stope boundaries to prevent dilution (Source_Document_Name_3)
- - Stabilizing mine intersections (Source_Document_Name_3)
- - Provide the ‘holding’ component of ground support systems in high stress brittle rock masses (Source_Document_Name_4)
+ - Stabilizing stope boundaries to prevent dilution (Source_Document_Title_3)
+ - Stabilizing mine intersections (Source_Document_Title_3)
+ - Provide the ‘holding’ component of ground support systems in high stress brittle rock masses (Source_Document_Title_4)
 
 Example 3:
 
@@ -182,13 +182,13 @@ How is rock mass spalling different than shearing?
 Answer:
 Spalling and shearing represent two fundamentally different failure processes observed in rock masses, particularly in the context of underground excavations and tunnel designs. Confusion often arises because shearing can occur along joints, through weak rock, and through brittle rock at high confining pressures.  
 
-Spalling is characterized by extensional fracturing or cracking through the rock, leading to the formation of slabs (Source_Document_Name_1). This process typically occurs under conditions of low confinement and is more prevalent in massive (high Geological Strength Index, GSI) and brittle rock masses (Source_Document_Name_2). 
+Spalling is characterized by extensional fracturing or cracking through the rock, leading to the formation of slabs (Source_Document_Title_1). This process typically occurs under conditions of low confinement and is more prevalent in massive (high Geological Strength Index, GSI) and brittle rock masses (Source_Document_Title_2). 
 
-Shearing in massive but weak rock masses is commonly observed because weak rocks have shear strengths that are less than their tensile strengths. Therefore, rather than extensional fracturing in compression, these weak rocks are more prone to shear. At very low strength, the shearing of weak rock is similar to shearing in soils (Source_Document_Name_3).
+Shearing in massive but weak rock masses is commonly observed because weak rocks have shear strengths that are less than their tensile strengths. Therefore, rather than extensional fracturing in compression, these weak rocks are more prone to shear. At very low strength, the shearing of weak rock is similar to shearing in soils (Source_Document_Title_3).
 
-Shearing in jointed rock masses does not primarily involve extensional fracturing or cracking through the rock itself, which can be strong and brittle. Rather, the displacements occurs predominantly along the joint planes which is influenced by the shear strength of the joint surfaces (Source_Document_Name_4). As GSI becomes lower, there is a transition in behavior from dominantly spalling to joint shear. GSI of 65 is a commonly recognized boundary for the transition from spalling to shear along joints (Source_Document_Name_3). 
+Shearing in jointed rock masses does not primarily involve extensional fracturing or cracking through the rock itself, which can be strong and brittle. Rather, the displacements occurs predominantly along the joint planes which is influenced by the shear strength of the joint surfaces (Source_Document_Title_4). As GSI becomes lower, there is a transition in behavior from dominantly spalling to joint shear. GSI of 65 is a commonly recognized boundary for the transition from spalling to shear along joints (Source_Document_Title_3). 
 
-Shearing in massive brittle rock (shear rupture), can occur under high confining stresses and there is a recognized transition from spalling to shearing that is referred to as the spalling limit, expressed as a ratio of the maximum to minimum principal stresses. The ratio typically ranges from 10 to 20 (Source_Document_Name_5). The shear rupture process involves the creation of initial en échelon fracture system arrays, which are predominately of a tensile mechanism at their time of creation. As the shear deformation continues, fractures propagate from the tips of the fractures in the en échelon arrays. After a period of applied displacement, these tip fractures stop growing, and new shallower angle fracture systems interconnect the initial arrays of en échelon fractures, creating a continuous horizontal displacement fracture that accommodates all the displacements associated with the applied shear (Source_Document_Name_5).  
+Shearing in massive brittle rock (shear rupture), can occur under high confining stresses and there is a recognized transition from spalling to shearing that is referred to as the spalling limit, expressed as a ratio of the maximum to minimum principal stresses. The ratio typically ranges from 10 to 20 (Source_Document_Title_5). The shear rupture process involves the creation of initial en échelon fracture system arrays, which are predominately of a tensile mechanism at their time of creation. As the shear deformation continues, fractures propagate from the tips of the fractures in the en échelon arrays. After a period of applied displacement, these tip fractures stop growing, and new shallower angle fracture systems interconnect the initial arrays of en échelon fractures, creating a continuous horizontal displacement fracture that accommodates all the displacements associated with the applied shear (Source_Document_Title_5).  
 
 In summary, spalling is associated with extensional fracturing and occurs under low confinement in massive and brittle rock masses. Shearing, conversely, is related to a different failure mechanism that occurs under different stress conditions and does not primarily involve extensional fracture growth. Distinctions between weak rock shearing, jointed shearing, and brittle shearing are crucial for understanding the failure processes in rocks, especially in the design and analysis of underground excavations and tunnels. 
 
